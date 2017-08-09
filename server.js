3d3e9831fe344e62ebf73bb876a372fe2a3c4be5
mijara <marjara35@gmail.com>
2017-08-08 23:28:13 -0400

    Remove livereload

--- server.js
+++ server.js
@@ -1,12 +1,8 @@
 const express = require('express')
-const livereload = require('express-livereload')
 const app = express()
-const http = require('http')
 
-livereload(app)
+app.use('/', express.static('public'))
 
-app.use('/', express.static('public'));
-
-http.createServer(app).listen(8080, function () {
+app.listen(8080, function () {
   console.log('Example app listening on port 8080!')
 })
