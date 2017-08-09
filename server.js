57595abaee089a1a7081bdc6eb595a3b41bba61c
mijara <marjara35@gmail.com>
2017-08-08 23:25:56 -0400

    Express server

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express')
+const livereload = require('express-livereload')
 const app = express()
+const http = require('http')
 
-app.use('/', express.static('public'))
+livereload(app)
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 3000!')
+app.use('/', express.static('public'));
+
+http.createServer(app).listen(8080, function () {
+  console.log('Example app listening on port 8080!')
 })
